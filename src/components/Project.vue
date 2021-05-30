956be9e9626a92833d086fdce23b5621f9556d66
Gerson Umanzor <gbumanzor@outlook.com>
2021-05-29 21:52:42 -0600

    Feat: Create project card component.

---
+++ src/components/Project.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="project">
+    <img src="https://picsum.photos/200/200" alt="" />
+    <h3>Decisionlink - Maui</h3>
+    <p>
+      SaaS for Value Cloud Management. Value Case, Hypotheses and Realizations
+      handling as well as Models, Factors, Benefits and Case Studies.
+    </p>
+    <div class="actions">
+      <a
+        href="https://github.com/gbumanzordev"
+        target="_blank"
+        class="bg-white rounded-full text-gray-900"
+      >
+        <i class="fab fa-github" />
+      </a>
+      <a
+        href="https://twitter.com/gbumanzordev"
+        target="_blank"
+        class="bg-white rounded-full text-gray-900"
+      >
+        <i class="fab fa-twitter" />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Project",
+};
+</script>
+
+<style scoped lang="scss">
+.project {
+  @apply flex flex-col shadow-lg rounded-lg hover:shadow-xl;
+}
+
+img {
+}
+
+h3 {
+  @apply py-4 text-xl font-bold uppercase overflow-ellipsis whitespace-nowrap overflow-x-hidden px-4;
+}
+
+p {
+  @apply text-lg px-4 mb-4;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+}
+
+.actions {
+  @apply px-4 mb-4 flex justify-center;
+
+  a {
+    @apply text-2xl;
+  }
+
+  a + a {
+    @apply ml-4;
+  }
+}
+</style>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,19 +1,42 @@
 <template>
-  <div class="wrapper">
+  <article class="wrapper">
     <Header>Projects I've worked on</Header>
-  </div>
+    <transition-group tag="div" class="list">
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+    </transition-group>
+  </article>
 </template>
 
 <script>
 import Header from "@/components/shared/Header";
+import Project from "@/components/Project";
 export default {
   name: "Portfolio",
-  components: { Header },
+  components: { Project, Header },
 };
 </script>
 
 <style scoped lang="scss">
 .wrapper {
-  @apply pt-4 px-4;
+  @apply pt-4 px-4 pb-8;
+}
+
+h1 {
+  @apply mb-8;
+}
+
+.list {
+  @apply grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-8;
 }
 </style>
