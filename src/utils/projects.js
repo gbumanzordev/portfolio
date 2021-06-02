0dfb202f71525ec4e3bca7acc44df7dd1897bf7c
Gerson Umanzor <gbumanzor@outlook.com>
2021-06-01 18:29:08 -0600

    Feat: Create reusable project list.

---
+++ src/utils/projects.js
@@ -0,0 +1,27 @@
+export const projects = [
+  {
+    name: "Decisionlink - Value Cloud",
+    photo: require("@/assets/dl.png"),
+    description:
+      "Value Cloud SaaS to manage Cases, Hypotheses, Realizations, Case Studies and Models.",
+    url: "https://demo.maui-dev.value-cloud.com",
+    stack: ["Angular", "NgRx", "Laravel"],
+  },
+  {
+    name: "Vue-Gi-Oh!",
+    photo: require("@/assets/vgo.png"),
+    description:
+      "Small pet project to practice my Vue skills, retrieves, paginates and shows details for the Yu-Gi-Oh! Cards.",
+    githubRepo: "https://github.com/gbumanzordev/vue-gi-oh",
+    url: "https://adoring-dijkstra-dafe6b.netlify.app/",
+    stack: ["Vue"],
+  },
+  {
+    name: "Gatekeeper",
+    photo: require("@/assets/gk.png"),
+    description:
+      "Single-Sign-On Application to handle authentication and sync for Applaudo Studios Employees.",
+    url: "https://gatekeeper.applaudostudios.com",
+    stack: ["Angular", "NgRx", "NestJS"],
+  },
+];
