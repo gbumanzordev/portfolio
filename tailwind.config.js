ef8aa8089d1741306e4e4d09b835ef8aee2a1d06
Gerson Umanzor <gbumanzor@outlook.com>
2021-06-07 15:44:37 -0600

    Fix: Update config for colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
     extend: {},
   },
   variants: {
