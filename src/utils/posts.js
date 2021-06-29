e84dff9324d081c02e859be3232ccd09bb383a9b
Gerson Umanzor <gbumanzor@outlook.com>
2021-06-28 22:30:51 -0600

    Fix: Add notice in blog section.

--- src/utils/posts.js
+++ src/utils/posts.js
@@ -2,11 +2,11 @@ export const blogPosts = [
   {
     id: 1,
     title: "Buenas Prácticas en Angular",
-    excerpt: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. A cupiditate
-      eius eum explicabo, illo magni necessitatibus sed tempora tenetur! Aliquam
-      assumenda at aut commodi dolorum ducimus enim, est explicabo fuga ipsa
-      iure modi necessitatibus nisi nulla repudiandae sint tenetur totam. Autem
-      dicta et fugiat iusto magnam nesciunt possimus praesentium vitae!`,
+    excerpt: `En esta publicacion les voy a hablar de buenas practicas y seguridad en Angular. 
+    Cuando estamos construyendo aplicaciones con Angular (o cualquier otro framework de Front-End, 
+    es importante mantener buenas practicas y asegurar la aplicación tanto como se pueda. Por eso, 
+    les voy a hablar de las buenas practicas recomendadas y las estrategias de seguridad mas importantes 
+    de aplicar en nuestro desarrollo.`,
   },
   {
     id: 2,
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="blog">
     <Header>My Blog</Header>
+    <h3 class='my-6 text-lg text-red-900'>
+      <b>Notice:</b> This section of my portfolio is a work in progress, none of
+      the posts in the list are published in this site, yet. But they will be
+      soon.
+    </h3>
     <BlogPost v-for="post of posts" :key="post.id" :post="post" />
   </div>
 </template>
